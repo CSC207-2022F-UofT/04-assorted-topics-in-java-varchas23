d2232c2175fb74367f1185764f5bdefe2cd34c62
Varchas Sharma <44586839+varchas23@users.noreply.github.com>
2022-10-09 22:21:43 -0400

    Update Trader.java

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -39,7 +39,10 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
-
+    public Trader(int money){
+        this.inventory = new ArrayList<>();
+        this.wishlist = new ArrayList<>();
+    }
 
 
 
@@ -47,8 +50,9 @@ public class Trader<T> {
     /* TODO: Implement the method addToWishlist that takes an
      *       object of type T and adds it to this Trader's wishlist.
      */
-
-
+    public void addToWishlist(T i){
+        this.wishlist.add(i);
+    }
 
 
 
@@ -59,7 +63,12 @@ public class Trader<T> {
      *
      *       We will call this in exchangeMoney().
      */
-
+    public int getSellingPrice(T obj){
+        if (obj instanceof Tradable){
+            return ((Tradable) obj).getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
 
 
